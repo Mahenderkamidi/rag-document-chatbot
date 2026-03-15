054b0d77af263f73f16e27cea2d1fb2e677b5cd3
mahender k <mahenderreddykamidi121@gmail.com>
2026-03-15 18:56:48 -0400

    Add basic chatbot functionality in app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+print("Hello! This is my RAG chatbot project.")
+question = input("Ask a question: ")
+
+if "ai" in question.lower():
+    print("Artificial Intelligence is technology that allows machines to learn.")
+else:
+    print("Sorry, I don't know that yet. This is version 1 of my chatbot.")
